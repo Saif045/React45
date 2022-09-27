f8577e32a9a94e46f04729e6d58aa89df9a2c147
Saif045 <saif.x45@gmail.com>
2022-09-27 21:11:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+
+      <Hero />
+
+      <div>sdad</div>
+      <Analytics />
+
+      <Newsletter />
+
+      <Cards />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
