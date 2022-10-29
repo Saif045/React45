2d861fff7296d0342802c8b55f6bd5b0d879ab9c
Saif045 <saif.x45@gmail.com>
2022-10-29 05:43:29 +0200

    fsa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Typed from 'react-typed'
 const Hero = () => {
   return (
-    <div className=' text-white'>
+    <div className=' text-white mt-40 h-screen '>
 
-       <div className=' max-w-[800px] mt-[-96px] w-full h-screen  mx-auto text-center flex flex-col justify-center'>
+       <div className=' max-w-[800px] mt-[-96px] w-full h-full  mx-auto text-center flex flex-col justify-center'>
         
          <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
          <h1 className=' md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
@@ -14,7 +14,7 @@ const Hero = () => {
             <Typed className=' md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={['BTB', 'BTC', 'SASS']} typedSpeed={120} backSpeed={140} loop />
           </div>
              
-            <p className=' md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, SASS platforms.</p>
+            <p className='  mx-8 sm:mx-16 md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, SASS platforms.</p>
             <button className=' bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
        </div>
     </div>
